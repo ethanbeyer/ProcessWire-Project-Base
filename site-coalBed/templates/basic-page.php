faca3d1e68b7149018b39ab6f8bc248f64cdb97a
Ethan Beyer <ethan.beyer@gmail.com>
2017-06-01 15:16:25 -0400

    Template Folder Updates

--- site-coalBed/templates/basic-page.php
+++ site-coalBed/templates/basic-page.php
@@ -1,13 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-		<title><?php echo $page->title; ?></title>
-		<link rel="stylesheet" type="text/css" href="<?php echo $config->urls->templates?>styles/main.css" />
-	</head>
-	<body>
-		<h1><?php echo $page->title; ?></h1>
-		<?php if($page->editable()) echo "<p><a href='$page->editURL'>Edit</a></p>"; ?>
-	
-	</body>
-</html>
+<?php
+
+// Class Use Calls
+
+/*
+page_fieldNames
+
+*****[otherPage]*****
+other_fieldName
+
+/*
+|==========================================================================
+| Functions
+|==========================================================================
+*/
+
+
+/*
+|==========================================================================
+| Variables
+|==========================================================================
+*/
+// $helpers->dd($var);
+
+/*
+|==========================================================================
+| View
+|==========================================================================
+*/
+?>
+
+<?
+//
+// Section Name
+//
+?>
